0991980318cc7d8da206e15fef7b4d298dff74ec
thaivan <giacatluong20121996@gmail.com>
2021-02-01 11:14:05 +0700

    convert JA-VI

--- bot.js
+++ bot.js
@@ -4,8 +4,14 @@
 const { ActivityHandler, MessageFactory, TurnContext } = require('botbuilder');
 const TranslateStrategy = require('./TranslateStrategy');
 
-const JAPANESE = 'ja';
-const VIETNAMESE = 'vi';
+const JAPANESE_CODE = 'ja';
+const VIETNAMESE_CODE = 'vi';
+
+const PREFIX_JAPAN = `#ja#`;
+const PREFIX_VIETNAM = `#vi#`;
+
+const TO_JAPAN = {from: VIETNAMESE_CODE, to: JAPANESE_CODE};
+const TO_VN = {from: JAPANESE_CODE, to: VIETNAMESE_CODE}
 
 const translator = require('@vitalets/google-translate-api');
 
@@ -17,13 +23,11 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            // TranslateStrategy.translate(context.activity.text, VIETNAMESE, JAPANESE, afterTranslate.bind(null, context, next));
-
             const currentUser = context.activity.from.id;
             conversationReferences[currentUser] = TurnContext.getConversationReference(context.activity);
             adapter = context.adapter;
 
-            translator(context.activity.text, {from: VIETNAMESE, to: JAPANESE})
+            translator(context.activity.text, this.detectLanguage(context.activity.text))
             .then(async (res) => {
                 await adapter.continueConversation(conversationReferences[currentUser], async turnContext => {
                     await turnContext.sendActivity(res.text);
@@ -45,6 +49,15 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    detectLanguage(text) {
+
+        if (text.includes(PREFIX_VIETNAM)) {
+            return TO_JAPAN;
+        }
+
+        return text.includes(PREFIX_JAPAN) ? TO_VN : TO_JAPAN
     }
 }
 
